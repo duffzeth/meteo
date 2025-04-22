d60181c7811fa0ec53fb6b1447349bdf895f69f8
duffzeth <ludovick.garcia@gmail.com>
2025-04-22 10:29:44 +0100

    Update consulta_tafor_aemet.py

--- consulta_tafor_aemet.py
+++ consulta_tafor_aemet.py
@@ -55,10 +55,9 @@ if consultar:
     response = requests.get(url)
     if response.status_code == 200:
         response_json = response.json()
-        
         if "data_day" in response_json and "data_1h" in response_json:
-    data_day = response_json["data_day"]
-    data_1h = response_json["data_1h"]
+        data_day = response_json["data_day"]
+        data_1h = response_json["data_1h"]
 
     # Convertir a DataFrame para facilitar manejo horario
     df_hourly = pd.DataFrame({
