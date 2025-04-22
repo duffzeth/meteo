dee2b4c5682b45b4ad529cb7551e921296919adf
duffzeth <ludovick.garcia@gmail.com>
2025-04-22 11:15:33 +0100

    Update aemet.py

--- aemet.py
+++ aemet.py
@@ -6,7 +6,7 @@ import pandas as pd
 # === Configuración ===
 #Gqtw5BUEcQDIk4eb
 #1uZEsTaN95h6qf0v
-API_KEY = "1uZEsTaN95h6qf0v"
+API_KEY = "gVphN31ARLcKGzX7"
 BASE_URL = "https://my.meteoblue.com/packages/basic-1h_basic-day_clouds-1h_clouds-day"
 
 aeropuertos = {
