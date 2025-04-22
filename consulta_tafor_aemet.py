3370d0f36f021881ce67dec2e36aac48f8ba4b23
duffzeth <ludovick.garcia@gmail.com>
2025-04-22 09:46:49 +0100

    Update consulta_tafor_aemet.py

--- consulta_tafor_aemet.py
+++ consulta_tafor_aemet.py
@@ -46,7 +46,7 @@ if consultar:
                 "Temp. mín (°C)": data["temperature_min"],
                 "Temp. media (°C)": data["temperature_mean"],
                 "Viento medio (kt)": [round(v * 1.94384, 1) for v in data["windspeed_mean"]],
-                "Dirección viento (°)": date["winddirection=2char"],
+                "Dirección viento (°)": date["winddirection"],
                 #"Nubosidad (%)": get_field("cloudcover"),
                 #"Techo nubes (m)": get_field("cloudbase_mean"),
                 "Precipitación (mm)": data["precipitation"],
